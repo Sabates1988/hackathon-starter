e6300c6f045a6f9e8793211e660aba345c2f5026
Arturo Sabates <sabates1988@outlook.com>
2020-04-28 23:35:04 -0400

    Test

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.84.183.96',
+      host: '3.85.185.217',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
